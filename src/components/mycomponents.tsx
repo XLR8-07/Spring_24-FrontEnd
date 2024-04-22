d4fa8108139526c9e6e88ecc906c147c2d6bcf01
XLR8-07 <joyashikur@gmail.com>
2024-04-22 10:28:23 +0600

    issue resolved with package conflicts

--- src/components/mycomponents.tsx
+++ src/components/mycomponents.tsx
@@ -1,43 +1,42 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 import axios from "axios";
 
-const NewComponent = () =>{
+const NewComponent = () => {
 
-    const [name , setName] = useState("")
-    const [country, setCountry] = useState("")
+    const [name, setName] = useState("")
+    const [country, setCountry] = useState(" ")
 
-    const handleNameInput = (value : string) =>{
+    const handleNameInput = (value: string) => {
         setName(value)
     }
 
-    const handleSubmit = async () =>{
+    const handleSubmit = async () => {
         console.log(name)
         const request_url = `https://api.nationalize.io/?name=${name}`
         const response = await axios.get(request_url)
-        if(response.data.country.length)
-        {
+        if (response.data.country.length) {
             setCountry(response.data.country[0].country_id)
         }
     }
     return (
         <View style={style.container}>
             <Text style={style.countryContainer}>{country}</Text>
-            <TextInput value={name} onChangeText={handleNameInput} style={style.inputContainer} placeholder="Enter Your Name"/>
-            <button onClick={handleSubmit} style={style.buttonContainer}>Submit</button>
+            <TextInput value={name} onChangeText={handleNameInput} style={style.inputContainer} placeholder="Enter Your Name" />
+            <Button onPress={handleSubmit} title="Submit"></Button>
         </View>
     )
 }
 
 const style = StyleSheet.create({
-    container:{
+    container: {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         marginTop: 150
     },
-    buttonContainer : {
+    buttonContainer: {
         margin: 10,
         width: 90,
         height: 40
@@ -47,7 +46,7 @@ const style = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 8
     },
-    countryContainer :{
+    countryContainer: {
         fontSize: 40,
         fontWeight: '700'
     }
